fa2e1d46a89d42ff39b5d330a6555725e16504aa
PashaEfimchik <pefimchshiks@gmail.com>
2023-10-10 16:56:44 +0300

    fixed updated time

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -6,13 +6,21 @@ interface TitleProps {
 
 const Title: React.FC<TitleProps> = ({ title }) => {
     const currentDateTime = new Date();
-    const formattedDateParts = currentDateTime.toLocaleDateString().toString().split('.');
-    const formattedDate = `${formattedDateParts[1]}/${formattedDateParts[0]}/${formattedDateParts[2]}`;
-    const formattedTime = currentDateTime.toLocaleTimeString();
+    const options: Intl.DateTimeFormatOptions = {
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    };
+
+    const formattedDate = currentDateTime.toLocaleDateString('en-US', options);
+
     return (
         <div className="title__container">
             <div className="title__container--content">{title}</div>
-            <div className="title__container--content-updated">Updated: {formattedDate} {formattedTime}</div>
+            <div className="title__container--content-updated">Updated: {formattedDate}</div>
         </div>
     );
 }
